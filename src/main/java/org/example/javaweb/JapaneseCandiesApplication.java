cfce5138ef9139d69079c37992905307c076befc
Assemgul Kakazhanova <kakazass@fel.cvut.cz>
2025-11-16 20:49:14 +0100

    Initial commit: Spring Boot Japanese Candies project

---
+++ src/main/java/org/example/javaweb/JapaneseCandiesApplication.java
@@ -0,0 +1,13 @@
+package org.example.javaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JapaneseCandiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JapaneseCandiesApplication.class, args);
+    }
+
+}
